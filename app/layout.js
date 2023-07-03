de07fb3696078d40873c67c59c2134cfb42e2ada
DavidWangwjh <davidwang.wjh@gmail.com>
2023-07-03 03:56:00 -0700

    updated head

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Where2Be | Find Events Here',
   description: 'Created by MomentEvents.',
+  robots: "all",
+  canonical: "https://where2be.app"
 }
 
 export default function RootLayout({ children }) {
