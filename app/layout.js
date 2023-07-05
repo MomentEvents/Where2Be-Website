c6fce6f7d6aad35c705f2a3ead74373d7ef5169b
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-05 09:03:28 -0700

    #5

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
-export const metadata = {
+const metadata = {
   author: "Where2Be",
   url: "Where2Be.app",
   title: "Where2Be | Your central school events hub",
@@ -9,31 +9,26 @@ export const metadata = {
     "Navigate your university experience with ease - Discover, Plan, and Connect with events around campus, all from your mobile device",
   image: "https://i.imgur.com/2iOelx1.png",
   robots: "all",
-  keywords: "Where2Be, School, Events"
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="robots" content={metadata.robots} />
-        <link rel="canonical" href="https://where2be.app" />
-        <meta property="og:url" content="https://where2be.app" />
-        <meta property="og:type" content="website" />
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="author" content={metadata.author} />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={metadata.image} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://where2be.app" />
-        <meta property="twitter:title" content={metadata.title} />
-        <meta property="twitter:description" content={metadata.description} />
-        <meta property="twitter:image" content={metadata.image} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:image" content={metadata.image} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="keywords" content="Where2Be, School, Events" />
         <meta property="image" content={metadata.image} />
-        <meta name="keywords" content="University, Events, Where2Be" />
-      </Head>
+        <title>{metadata.title}</title>
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
