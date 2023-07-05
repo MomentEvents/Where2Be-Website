85885eb4c37be8b4dab0a04fcd5773f36e703736
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-07-05 07:35:41 -0700

    Changed header for better SEO

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,32 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export const metadata = {
   title: 'Where2Be | Find school events',
-  description: 'A central hub for all your school events at UCSD and UIUC.',
+  description: 'A central hub for all your school events across multiple institutions.',
+  image: 'https://i.imgur.com/2iOelx1.png',
   robots: "all",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="robots" content={metadata.robots} />
         <link rel="canonical" href="https://where2be.app" />
-        <meta property="og:title" content="Where2Be" />
-        <meta
-          property="og:description"
-          content="A central hub for all your school events"
-        />
-        <meta
-          property="og:image"
-          content="https://i.imgur.com/2iOelx1.png"
-        />
-      </head>
+        <meta property="og:url" content="https://where2be.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://where2be.app" />
+        <meta property="twitter:title" content={metadata.title} />
+        <meta property="twitter:description" content={metadata.description} />
+        <meta property="twitter:image" content={metadata.image} />
+      </Head>
       <body>{children}</body>
     </html>
   )
