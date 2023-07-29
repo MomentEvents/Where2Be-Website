f20f902bc23b51ef97eb5cf3659e7a27366447e6
DavidWangwjh <davidwang.wjh@gmail.com>
2023-07-29 03:18:39 -0700

    updated icons

--- app/event/[eventId]/page.js
+++ app/event/[eventId]/page.js
@@ -6,76 +6,110 @@ import "./event.css";
 import "../../home.css";
 import "@/components/index.css"
 import { Date, Time } from '@/components/DateTime/datetime';
-import { CalendarIcon, LocationIcon, TimeIcon, VisibilityIcon } from "@/assets/eventIcons";
 import { Navbar, Footer } from "@/components/index";
-
+import RedirectWindow from '@/components/RedirectWindow/RedirectWindow.jsx';
+import { FiCalendar, FiClock } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
+import { TbMapSearch } from "react-icons/tb";
+import { COLORS } from "@/constants/themes";
 
 export default async function Event({ params }) {
     const event = await getEvent(params.eventId);
 
     return (
         <>
-            {/* thumbnail */}
-            <Head>
-                <meta property="og:title" content={event.title} />
-                <meta property="og:description" content={event.description} />
-                <meta property="og:image" content={event.picture} />
-                <meta property="og:url" content={`https://where2be.app/event/${event.event_id}`} />
-            </Head>
+            {event? (
+                <>
+                    {/* thumbnail */}
+                    <Head>
+                        <meta property="og:title" content={event.title} />
+                        <meta property="og:description" content={event.description} />
+                        <meta property="og:image" content={event.picture} />
+                        <meta property="og:url" content={`https://where2be.app/event/${event.event_id}`} />
+                    </Head>
+                        
 
-            {/* navbar w/o menu */}
-            <header className="navbar-bg">
-                <Navbar showMenu={false}/>
-            </header>
+                    {/* navbar w/o menu */}
+                    <header className="navbar-bg">
+                        <Navbar showMenu={false}/>
+                    </header>
 
-            {/* event content */}
-            <div className="event-container" >
-                {/* left text container */}
-                <div className="event-text-container" >
-                    <h1 className="event-title">{event.title}</h1>
-                
-                    <div className="date-time-container">
-                        <div className="icon-text-container">
-                            <CalendarIcon />
-                            <div style={{ width: "10px" }}></div>
-                            <h4 className="u-text-small">
-                                <Date dateString={event.start_date_time} />
-                            </h4>
+                    {/* event content */}
+                    <div className="event-container" >
+                        {/* left text container */}
+                        <div className="event-text-container" >
+                            <h1 className="event-title">{event.title}</h1>
+                        
+                            <div className="date-time-container">
+                                <div className="icon-text-container" style={{ marginRight: "30px" }}>
+                                    <FiCalendar 
+                                        size={30}
+                                        color={COLORS.purple}
+                                        style={{ marginRight: 10 }}
+                                    />
+                                    
+                                    <h4 className="u-text-small">
+                                        <Date dateString={event.start_date_time} />
+                                    </h4>
+                                </div>
+                                <div className="icon-text-container">
+                                    <FiClock 
+                                        size={30}
+                                        color={COLORS.purple}
+                                        style={{ marginRight: 10 }}
+                                    />
+                                    <h4 className="u-text-small">
+                                        <Time timeString={event.start_date_time} /> - <Time timeString={event.end_date_time} />
+                                    </h4>
+                                </div>
+                            </div>
+                            
+                            <div className="icon-text-container">
+                                <IoLocationOutline 
+                                    size={30}
+                                    color={COLORS.lightGray}
+                                    style={{ marginRight: 10 }}
+                                />
+                                <h4 className="u-text-small">{event.location}</h4>
+                            </div>
+
+                            <h4 className="u-text-small description">{event.description} </h4>
+
+                            {event.interests.map((interest) => (
+                                <div className="interest" key={interest}>
+                                    <h4 className="u-text-small">{interest}</h4>
+                                </div>
+                            ))}
+                            
+                            <div className="icon-text-container" style={{ marginTop: '1.5rem' }}>
+                                <TbMapSearch 
+                                    size={30}
+                                    color={COLORS.lightGray}
+                                    style={{ marginRight: 10 }}
+                                />
+                                <h4 className="u-text-small">{event.visibility}</h4>
+                            </div>
                         </div>
-                        <div style={{ width: "30px" }}></div>
-                        <div className="icon-text-container">
-                            <TimeIcon />
-                            <div style={{ width: "10px" }}></div>
-                            <h4 className="u-text-small">
-                                <Time timeString={event.start_date_time} /> - <Time timeString={event.end_date_time} />
-                            </h4>
+
+                        {/* right image container */}
+                        <div className="event-image-container" >
+                            <Image 
+                                src={event.picture} 
+                                alt="Event Image" 
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                }}
+                            />
                         </div>
                     </div>
-                    
-                    <div className="icon-text-container">
-                        <LocationIcon />
-                        <div style={{ width: "10px" }}></div>
-                        <h4 className="u-text-small">{event.location}</h4>
-                    </div>
 
-                    <h4 className="u-text-small description">{event.description} </h4>
-                </div>
-
-                {/* right image container */}
-                <div className="event-image-container" >
-                    <Image 
-                        src={event.picture} 
-                        alt="Event Image" 
-                        style={{
-                            width: '100%',
-                            height: 'auto',
-                        }}
-                    />
-                </div>
-            </div>
-
-            {/* footer */}
-            <Footer />
+                    {/* footer */}
+                    <Footer />
+                </>
+            ) : (
+                <RedirectWindow url={"https://where2be.app/404"}/>
+            )}
         </>
     );
 }
@@ -83,7 +117,7 @@ export default async function Event({ params }) {
 export async function getEvent( eventId ) {
   
     try {
-        // const response = await fetch(`https://where2be.app/event/event_id/${eventId}`);
+        // const response = await fetch(`https://where2be.app/event/${eventId}`);
         // const event = await response.json();
 
         const event = {
@@ -94,7 +128,8 @@ export async function getEvent( eventId ) {
             'location': 'Geisel',
             'start_date_time': "2023-06-16T17:00:00Z",
             'end_date_time': "2023-06-16T18:00:00Z",
-            'visibility': true
+            'visibility': "Public",
+            'interests': ['Academic', 'Athletic']
         };
 
         return event;
